8dc334712f00686a25fda3070ef0e6e17616e019
Alex de Cock Buning <100383348@alumnos.uc3m.es>
2024-10-02 04:58:56 +0200

    added dockerfile

---
+++ build/build-go.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+DEST_DIR="/usr/local/bin"
+
+
+if [ ! -d ${DEST_DIR} ]; then
+	mkdir ${DEST_DIR}
+fi
+
+go build -v -o "${DEST_DIR}"/l2sm-init ./cmd/l2sm-init 
+go build -v -o "${DEST_DIR}"/l2sm-vxlans ./cmd/l2sm-vxlans 
+go build -v -o "${DEST_DIR}"/l2sm-add-port ./cmd/l2sm-add-port 
+go build -v -o "${DEST_DIR}"/ned-server ./cmd/ned-server
