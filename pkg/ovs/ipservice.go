85db57f0f4ce0cc3452dde19034618e3bd99524c
Tjaarda1 <abuning@pa.uc3m.es>
2026-02-12 22:47:44 -0800

    refactor(ovs): remove deprecated AddInterfaceToBridge/ipservice bridge-veth helpers

--- pkg/ovs/ipservice.go
+++ pkg/ovs/ipservice.go
@@ -36,23 +36,3 @@ func (ipService *IpService) AddIpAddress(interfaceName string, ip netip.Prefix) 
 	}
 	return nil
 }
-
-func (ipService *IpService) AddVethPair(vethName, peerName string) error {
-
-	o, err := ipService.exec.CombinedOutput("link", "add", vethName, "type", "veth", "peer", "name", peerName)
-	if err != nil {
-		return fmt.Errorf("command error: %v\nOutput: %s", err, o)
-	}
-	return nil
-
-}
-
-func (ipService *IpService) AddInterfaceToLinuxBridge(interfaceName, bridgeName string) error {
-
-	o, err := ipService.exec.CombinedOutput("link", "set", interfaceName, "master", bridgeName)
-	if err != nil {
-		return fmt.Errorf("command error: %v\nOutput: %s", err, o)
-	}
-	return nil
-
-}
